8337fdaee2d3a1253be80d36017db25d1eb58853
nderhore <nathan@derhore.fr>
2023-12-12 19:38:06 +0100

    implements Spring UserDetails implementation

---
+++ src/main/java/com/studi/library/security/service/UserDetailsImpl.java
@@ -0,0 +1,68 @@
+package com.studi.library.security.service;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.studi.library.pojo.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+public class UserDetailsImpl implements UserDetails {
+
+    private String username;
+
+    @JsonIgnore
+    private String password;
+
+    private Collection<? extends GrantedAuthority> authorities;
+
+    public UserDetailsImpl(String username, String password,
+                           Collection<? extends GrantedAuthority> authorities){
+        this.username = username;
+        this.password = password;
+        this.authorities = authorities;
+
+    }
+
+    public static UserDetails build(User user) {
+        return new UserDetailsImpl(
+                user.getUsername(),
+                user.getPassword(),
+                null);
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
