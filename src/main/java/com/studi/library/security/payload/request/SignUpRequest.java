3b10e7b9cadecfa92533ed1fe5907d612f7823a1
nderhore <nathan@derhore.fr>
2023-12-12 19:36:23 +0100

    implements models for endpoint jwt

---
+++ src/main/java/com/studi/library/security/payload/request/SignUpRequest.java
@@ -0,0 +1,21 @@
+package com.studi.library.security.payload.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SignUpRequest {
+
+    @NotBlank
+    @Size(min = 8, max = 40)
+    @Pattern(regexp = "(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%&]).{10,}", message = "Le mot de passe doit contenir au moins 8 caractères, une majuscule, une minuscule, un chiffre et un caractère spécial.")
+    private String password;
+
+    @NotBlank
+    @Size(min = 3, max = 50)
+    private String username;
+}
